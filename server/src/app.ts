fb60d41bb962d64efaefc766a52ce04dc4791daa
Hannah6898 <hannahosibodu68@outlook.com>
2022-09-24 16:09:48 +0100

    Setup configurations and server using express in app.ts file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,27 @@
-console.log("Hey there")
+import express from 'express'
+import { createServer } from 'http'
+import {Server} from 'socket.io'
+import cors from 'cors'
+import config from 'config'
+//Allows you to create json files
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors:{
+        origin: corsOrigin, 
+        credentials:true,
+    }
+});
+
+app.get('/', (req,res)=>{
+res.send('Hello');
+});
+
+httpServer.listen(port, host,()=> console.log('Server is listening'))
