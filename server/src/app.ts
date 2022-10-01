4da7bd45b60ffa691be95f543e27c7b848568b49
Hannah6898 <hannahosibodu68@outlook.com>
2022-10-01 11:21:37 +0100

    Setup Socket io

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,7 @@ import config from 'config'
 //Allows you to create json files
 import logger from '../utils/logger';
 import {version} from "../package.json";
+import socket from './socket'
 
 const port = config.get<number>("port");
 const host = config.get<string>("host");
@@ -29,4 +30,5 @@ res.send('Hello');
 httpServer.listen(port, host,()=> {
     logger.info(`🚀 Server version ${version} is listening 🚀`)
     logger.info(`http://${host}:${port}`)
+    socket({io});
 });
