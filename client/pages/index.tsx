e866015d703026355f66313e784e644dbbf56416
Hannah6898 <hannahosibodu68@outlook.com>
2022-11-26 12:04:04 +0000

    Added CSS for landing page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,7 +5,7 @@ import { useSockets } from "../context/socket.context";
 import MessagesContainer from "../containers/Messages";
 import RoomsContainer from "../containers/Rooms";
 import { useEffect, useRef } from "react";
-import LandingPageImg from "../public/LandingPageImg.jpeg";
+import LandingPageImg from "../public/Landing-Page-Img.png";
 
 export default function Home() {
   const { socket, username, setUsername } = useSockets();
@@ -20,30 +20,43 @@ export default function Home() {
     localStorage.setItem("username", value);
   }
 
-  useEffect(()=>{
-    if(usernameRef){
-      usernameRef.current.value = localStorage.getItem('username') || ''
+  useEffect(() => {
+    if (usernameRef) {
+      usernameRef.current.value = localStorage.getItem("username") || "";
     }
-  },[])
+  }, []);
 
   return (
-      <div >
+    <div>
       {!username && (
         <div className={styles.homeContainer}>
-        <img src={"../public/LandingPageImg.jpeg"}></img>
-        <div className={styles.heading}>WhatsApp for Desktop</div>
-        <br/>
-        <div>Send and receive messages without keeping your phone online.</div>
-        <div>Use WhatsApp on up to 4 linked devices and 1 phone at the same time.</div>
-        <br/>
-        <div className={styles.headingTwo}>Sign In</div>
-        <br/>
-        <div className={styles.usernameWrapper}>
-          <div className={styles.usernameInner}>
-          <input placeholder="Username" ref={usernameRef} className={styles.input}></input>
-          <button onClick={handleSetUsername}>START</button>
-        </div>
-        </div>
+          <Image
+            src={LandingPageImg}
+            alt="Landing page image"
+            height="300px"
+          ></Image>
+            <br />
+          <div className={styles.heading}>WhatsApp for Desktop</div>
+          <br />
+          <div>
+            Send and receive messages without keeping your phone online.
+          </div>
+          <div>
+            Use WhatsApp on up to 4 linked devices and 1 phone at the same time.
+          </div>
+          <br />
+          <div className={styles.headingTwo}>Sign In</div>
+          <br />
+          <div className={styles.usernameWrapper}>
+            <div className={styles.usernameInner}>
+              <input
+                placeholder="Username"
+                ref={usernameRef}
+                className={styles.input}
+              ></input>
+              <button onClick={handleSetUsername}>Sign In</button>
+            </div>
+          </div>
         </div>
       )}
 
@@ -53,6 +66,6 @@ export default function Home() {
           <MessagesContainer />
         </div>
       )}
-      </div>
+    </div>
   );
 }
